8dfa97520373f8ab596d4671cab231efbbee9341
Hemanka <hkanoujia02@gmail.com>
2024-08-05 00:02:06 -0400

    working pantry-tracker

--- app/page.js
+++ app/page.js
@@ -1,95 +1,223 @@
+"use client";
+
+import { Box, Button, Modal, Stack, TextField, Typography } from "@mui/material";
 import Image from "next/image";
-import styles from "./page.module.css";
+import { useState, useEffect } from "react";
+import {firestore} from '@/firebase'
+import { collection, getDocs, doc, getDoc, setDoc, deleteDoc, query } from "firebase/firestore";
 
 export default function Home() {
+  const [inventory, setInventory] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [newItem, setNewItem] = useState({ name: "", quantity: "" });
+  const [search, setSearch] = useState("");
+
+  const updateItems = async () => {
+    const snapshot = query(collection(firestore, "inventory"));
+    const docs = await getDocs(snapshot);
+    const inventoryList = [];
+    docs.forEach((doc) => {
+      inventoryList.push({
+        name:doc.id,
+        ...doc.data()
+      });
+    });
+    setInventory(inventoryList);
+  }
+
+  const removeItem = async (item) => {
+    const docRef = doc(firestore, "inventory", item);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      const {quantity} = docSnap.data();
+      if (quantity === 1) {
+        await deleteDoc(docRef);
+      } else {
+        await setDoc(docRef, {quantity: quantity - 1});
+      }
+    }
+
+    await updateItems();
+  }
+
+  const addItem = async (item) => {
+    const docRef = doc(firestore, "inventory", item);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      const {quantity} = docSnap.data();
+      await setDoc(docRef, {quantity: quantity + 1});
+    }
+    // else add the new iteam with the given quantity
+    else {
+      await setDoc(docRef, {quantity: newItem.quantity});
+    }
+
+    await updateItems();
+  }
+
+  useEffect(() => {
+    updateItems();
+  }, []);
+
+  const searchItems = (search) => {
+    if (search === "") {
+      updateItems();
+      // setInventory([
+      //   { name: "Apple", quantity: 5 },
+      //   { name: "Banana", quantity: 3 },
+      //   { name: "Orange", quantity: 7 },
+      //   { name: "Grapes", quantity: 2 },
+      //   { name: "Pineapple", quantity: 1 },
+      // ]);
+    } else {
+      console.log(search);
+      setInventory(inventory.filter((item) => item.name.toLowerCase().includes(search.toLowerCase())));
+    }
+  }
+
+  const handleOpen = () => {
+    setOpen(true);
+  }
+
+  const handleClose = () => {
+    setOpen(false);
+  }
+
+  const showInput = () => {
+    console.log(newItem);
+    console.log(inventory);
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <Box 
+      width="100vw"
+      height="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="column"
+    >
+      <Typography variant="h3">
+        Pantry Tracker
+      </Typography>
+      
+      <Modal open={open} onClose={handleClose}>
+        <Box
+          position="absolute"
+          top="50%"
+          left="50%"
+          width={400}
+          bgcolor="white"
+          border="2px solid black"
+          boxShadow={24}
+          p={4}
+          display="flex"
+          flexDirection="column"
+          gap={3}
+          sx={{
+            transform: "translate(-50%, -50%)"
+          }}
+        >
+          <Typography variant="h5">Add Item</Typography>
+
+          <Stack width="100%" direction="row" justifyContent="space-between" spacing={2}>
+            <Box width="75%">
+              <TextField
+                width="60%"
+                label="Item"
+                variant="outlined"
+                value={newItem.name}
+                onChange={(e) => setNewItem({ ...newItem, name: e.target.value })}
+              />
+            </Box>
+            <Box width="35%">
+              <TextField
+                width="25%"
+                label="Quantity"
+                variant="outlined"
+                value={newItem.quantity}
+                onChange={(e) => setNewItem({ ...newItem, quantity: e.target.value })}
+              />
+            </Box>
+          </Stack>
+
+          <Button onClick={() => {
+            addItem(newItem.name)
+            // showInput();
+            setNewItem({ name: "", quantity: "" });
+            handleClose();
+          }}>
+            Add
+          </Button>
+        </Box>
+      </Modal>
+
+      <Box border="1px solid #333" margin={5}>
+        <Box
+          bgcolor="#ADD8E6"
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between"
+          padding={3}
+        >
+          <Stack width="100%" direction="row" justifyContent="space-between" spacing={2}>
+            <Box width="90%">
+              <TextField
+                    fullWidth
+                    label="Search"
+                    variant="outlined"
+                    value={search}
+                    onChange={(e) => {
+                      setSearch(e.target.value);
+                      searchItems(e.target.value);
+                    }}
+                  />
+            </Box>
+            <Box width="10%" padding={1}>
+              <Button onClick={handleOpen}>Add</Button>
+            </Box>
+          </Stack>
+
+        </Box>
+      
+      <Stack width="800px" height="300px" spacing={2} overflow="auto">
+        { inventory.map((item) => (
+          <Box 
+            key={item.name}
+            width="100%"
+            height="50px"
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
+            padding={5}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+            <Typography variant="h5">{item.name}</Typography>
+            <Typography variant="h5">{item.quantity}</Typography>
+            <Stack direction="row" spacing={2}>
+            <Button
+                variant="contained"
+                onClick={() => {
+                  addItem(item.name)
+                }}
+              >
+                Add
+              </Button>
+              <Button
+                variant="contained"
+                onClick={() => {
+                  removeItem(item.name)
+                }}
+              >
+                Delete
+              </Button>
+            </Stack>
+          </Box>
+        ))}
+      </Stack>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </Box>
+    </Box>
   );
 }
